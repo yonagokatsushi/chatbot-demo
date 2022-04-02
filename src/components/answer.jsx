d95c224021b0f9cd5038ce99350ba06710b2184e
yonago_katsushi <yonago.katsushi@gmail.com>
2022-04-02 16:28:42 +0900

    実践編 5まで

--- src/components/answer.jsx
+++ src/components/answer.jsx
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 
 const Answer = (props) => {
     return (
-        <Button variant="contained">
+        <Button variant="contained" color='primary'>
 
             {props.content}
         </Button>
